2b3515c4fa6beae34fdfe632764d73cfe8565228
FernandoNM02 <feraxel15@gmail.com>
2022-11-08 20:51:35 -0600

    update

---
+++ Parcial3/MODULOCOMMOJS/app.js
@@ -0,0 +1,11 @@
+//const modulev = require("./modulo");
+
+const cowsay = require('cowsay');
+
+//console.log(modulo.cuadrado(2));
+
+console.log(cowsay.say({
+    text: " I M MODULE",
+    e:"00",
+    T:"UU"
+}));
