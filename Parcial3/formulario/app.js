80c09badec6d351f39f92ac9034c9813dce647bf
FernandoNM02 <feraxel15@gmail.com>
2022-11-17 21:21:57 -0600

    Update

--- Parcial3/formulario/app.js
+++ Parcial3/formulario/app.js
@@ -1,10 +1,3 @@
-/* document.getElementById("btnGet").addEventListener('click',async()=>{
-    let respuesta = await fetch('data.php');
-    let dato = await respuesta.json();
-    document.getElementById("NumeroTelefono").value = dato.phone;
-    document.getElementById("Nombres").value = dato.name;
-    
-      }); */
 
       document.getElementById('btnGet').addEventListener('click',async()=>{
         const data = await (await fetch('./data.php')).json()
